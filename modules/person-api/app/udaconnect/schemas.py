fecb63bcb1ec2073e9145053118414e09f68b7a9
sharathp <>
2021-08-13 22:41:50 -0400

    Adjusted controller schemas to reflect required vs optional fields

--- modules/person-api/app/udaconnect/schemas.py
+++ modules/person-api/app/udaconnect/schemas.py
@@ -1,11 +1,12 @@
 from app.udaconnect.models import Person
 from marshmallow import Schema, fields
 
+
 class PersonSchema(Schema):
     id = fields.Integer()
-    first_name = fields.String()
-    last_name = fields.String()
-    company_name = fields.String()
+    first_name = fields.String(required=True)
+    last_name = fields.String(required=True)
+    company_name = fields.String(required=True)
 
     class Meta:
         model = Person
